bcdacce6e695395d0a8353c182e1087d023876ee
khamdenglao22 <khamdenglao22@gmail.com>
2024-04-22 15:45:50 +0700

    icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,18 +4,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "ST-Muang Thai Insurance",
+  icons: {
+    icon: ["/logo.svg"],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link
-          rel="icon"
-          type="image/svg"
-          href="/logo.svg"
-          sizes="any"
-        />
         <link rel="stylesheet" href="/fonts/stylesheet.css" />
       </head>
       <body className={inter.className}>{children}</body>
